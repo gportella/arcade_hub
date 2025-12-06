56f9c97c8cb048a02c0845cd2cec65c16685784e
Guillem Portella <ktxc111@astrazeneca.net>
2025-12-06 21:53:17 +0000

    fix config more fix route

--- connect_4/frontend/vite.config.js
+++ connect_4/frontend/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [svelte()],
-})
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), 'VITE_')
+  const base = env.VITE_BASE_PATH || '/'
+
+  return defineConfig({
+    plugins: [svelte()],
+    base,
+  })
+}
--- solve_parking/frontend/vite.config.js
+++ solve_parking/frontend/vite.config.js
@@ -1,15 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [svelte()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:8000',
-        changeOrigin: true,
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), 'VITE_')
+  const base = env.VITE_BASE_PATH || '/'
+
+  return defineConfig({
+    plugins: [svelte()],
+    base,
+    server: {
+      proxy: {
+        '/api': {
+          target: 'http://127.0.0.1:8000',
+          changeOrigin: true,
+        },
       },
     },
-  },
-})
+  })
+}
